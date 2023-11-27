b82b21165bc980278fcf7e31e2ad2fa92af7050c
zhifan <81158160+zhifana@users.noreply.github.com>
2023-11-27 16:09:11 +0800

    Update 监控git仓库并发送通知.py

--- 自己用的一些小代码/监控git仓库并发送通知.py
+++ 自己用的一些小代码/监控git仓库并发送通知.py
@@ -4,12 +4,14 @@ import subprocess
 from discord_webhook import DiscordWebhook
 import logging
 
-# 设置日志级别为 INFO
-logging.basicConfig(level=logging.INFO)
-
 # Discord Webhook URL 和仓库路径作为参数传递
 DISCORD_WEBHOOK_URL = "你的DISCORD_WEBHOOK_URL"
 REPO_PATH = "/home/zhifan/bounty-targets-data" #你要监控的github仓库，需要克隆到本地
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+MAX_RETRIES = 3
+RETRY_INTERVAL = 60  # 重试间隔时间（秒）
 
 def git_pull(repo_path):
     try:
@@ -30,29 +32,27 @@ def get_git_updates(repo_path):
         logging.error(f"检索git更新时发生错误: {e}")
         raise
 
-def send_notification(commit_info, webhook_url):
+def send_notification(commit_info, webhook_url, retries=0):
     try:
-        # 设置 Discord Webhook
         webhook = DiscordWebhook(url=webhook_url)
-
-        # 分割消息内容
         max_length = 1800
         chunks = [commit_info[i:i + max_length] for i in range(0, len(commit_info), max_length)]
 
         for chunk in chunks:
-            # 将提交信息作为消息内容
             message_content = f"GitHub 仓库更新:\n```diff\n{chunk}\n```"
-
-            # 设置消息内容
             webhook.content = message_content
-
-            # 执行 Webhook
             webhook.execute()
 
         logging.info("成功发送 Discord 通知。")
     except Exception as e:
         logging.exception("发送 Discord 通知时发生错误")
-        raise
+
+        if retries < MAX_RETRIES:
+            logging.info(f"等待 {RETRY_INTERVAL} 秒后进行重试...")
+            time.sleep(RETRY_INTERVAL)
+            send_notification(commit_info, webhook_url, retries=retries + 1)
+        else:
+            logging.error("达到最大重试次数，放弃重试。")
 
 def update_and_notify():
     try:
@@ -65,21 +65,13 @@ def update_and_notify():
 
     except Exception as e:
         logging.exception(f"发生错误: {e}")
-        raise  # 将异常重新抛出，以便退出当前循环
 
-# 无限循环
+# 初始运行一次
+update_and_notify()
+
+# 设置每30分钟执行一次
+schedule.every(30).minutes.do(update_and_notify)
+
 while True:
-    try:
-        # 初始运行一次
-        update_and_notify()
-
-        # 设置每30分钟执行一次
-        schedule.every(30).minutes.do(update_and_notify)
-
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
-
-    except Exception as e:
-        logging.exception(f"主循环中发生错误: {e}")
-        logging.info("重新运行脚本...")
+    schedule.run_pending()
+    time.sleep(1)
